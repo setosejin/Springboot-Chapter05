9c84a87e378b2f1fde733c5fd9f6e55b169ffb1a
Sejin Kim <lemon05090@naver.com>
2022-06-23 10:22:10 +0900

    add Board Insert Test code

---
+++ src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.rubypaper.persistence;
+
+import com.rubypaper.domain.Board;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {
+    List<Board> findByTitle(String searchKeyword);
+}
